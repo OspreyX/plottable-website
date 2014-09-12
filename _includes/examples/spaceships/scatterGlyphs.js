b6e086134442a6b385e54c754e0444c88edc3761
Bill Dwyer <bdwyer@palantir.com>
2014-09-12 13:56:58 +0100

    refactoring and updating ship examples.

--- _includes/examples/spaceships/scatterGlyphs.js
+++ _includes/examples/spaceships/scatterGlyphs.js
@@ -1,26 +1,56 @@
-d3.csv("ships.csv", function(error, ships) {
-  ships = _(ships)
-    .sortBy("Race")
-    .forEach(function(ship, i){
-      ship.i = i;
-    })
-    .value();
+$.when(
+  $.getJSON("ships.json").then(_.identity),
+  $.getJSON("raceSchema.json").then(_.identity),
+  $.getJSON("hullSchema.json").then(_.identity)
+).then(function(ships, raceSchema, hullSchema){
+
+
+  // Returns the race for a given datum
+  function getRace(d) {
+    var race = d.Race.trim();
+    return _(raceSchema).keys().contains(race) ? race : "Pirate";
+  }
+
+  // Returns the hull size for a given datum
+  function getHullSize(d) {
+    var shipClass = _.find(hullSchema, function(hullClass){
+      return (hullClass.shipClasses.indexOf(d.Class) !== -1);
+    });
+    return shipClass.size;
+  }
+
+  // Assign keys for layout
+  _(ships)
+    .groupBy(getRace)
+    .forEach(function(ss, ssi){
+      ss.forEach(function(s, si){
+        s.raceKey = ssi;
+        s.shipKey = si;
+      });
+    });
 
   // Scales
-  var xScale     = new Plottable.Scale.Linear();
-  var yScale     = new Plottable.Scale.Linear();
-  var classScale = new Plottable.Scale.Ordinal();
-  var raceColors = new Plottable.Scale.Color();
+  var xScale    = new Plottable.Scale.Linear();
+  var yScale    = new Plottable.Scale.Ordinal()
+    .domain(_(raceSchema).keys().reverse().value());
+  var raceScale = new Plottable.Scale.Color()
+    .domain(_.keys(raceSchema))
+    .range(_.map(raceSchema, "color"));
+  var techScale = new Plottable.Scale.Color()
+    .domain(["1", "F", "2"])
+    .range(["black", "green", "orange"]);
 
   // Plot Components
   var title      = new Plottable.Component.TitleLabel("Ships");
-  var raceLegend = new Plottable.Component.HorizontalLegend(raceColors);
+  var raceLegend = new Plottable.Component.HorizontalLegend(raceScale);
   var plot       = new Plottable.Plot.Scatter(ships, xScale, yScale)
-    .project("x", "i", xScale)
-    .project("y", 0, yScale)
-    .project("stroke-width", "Ship class", classScale)
-    .project("fill", "Race", raceColors)
-    .project("r", "Turrets");
+    .project("x", "shipKey", xScale)
+    .project("y", "raceKey", yScale)
+    .project("r", getHullSize)
+    .project("stroke", "Tech", techScale)
+    .project("stroke-width", 2)
+    .project("opacity", 1)
+    .project("fill", getRace, raceScale);
 
   // Layout and render
   new Plottable.Component.Table([
