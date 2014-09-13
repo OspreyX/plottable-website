9c096ffab817e9ee123f9889a1eb6785d9e739fa
Daniel Mane <danmane@gmail.com>
2014-09-14 00:54:23 +0200

    Add extra download and examples link at bottom of page. Remove example chart.

--- _includes/examples/scatterplot/plottable.js
+++ _includes/examples/scatterplot/plottable.js
@@ -1,28 +1,27 @@
 d3.tsv("data.tsv", function(error, data) {
-  data.forEach(function(d) {
-     d.sepalLength = +d.sepalLength;
-     d.sepalWidth = +d.sepalWidth;
-   });
   var xScale = new Plottable.Scale.Linear();
   var yScale = new Plottable.Scale.Linear();
   var colorScale = new Plottable.Scale.Color("10");
 
   var xAxis = new Plottable.Axis.Numeric(xScale, "bottom");
   var yAxis = new Plottable.Axis.Numeric(yScale, "left");
+
   var xLabel = new Plottable.Component.Label("Sepal Width (cm)");
   var yLabel = new Plottable.Component.Label("Sepal Length (cm)", "left");
 
-  var legend = new Plottable.Component.Legend(colorScale);
-
   var plot = new Plottable.Plot.Scatter(data, xScale, yScale)
-                      .project("x", "sepalWidth", xScale)
-                      .project("y", "sepalLength", yScale)
-                      .project("fill", "species", colorScale);
+                      .attr("x", "sepalWidth", xScale)
+                      .attr("y", "sepalLength", yScale)
+                      .attr("r", 3.5)
+                      .attr("fill", "species", colorScale);
+
+  var legend = new Plottable.Component.Legend(colorScale);
 
   var center = plot.merge(legend);
   var table = new Plottable.Component.Table([
                 [yLabel, yAxis, center],
-                [null  , null , xAxis],
-                [null  , null , xLabel]
-        ]).renderTo(d3.select("#scatterplot-plottable-demo"));
+                [null  , null , xAxis ],
+                [null  , null , xLabel]]
+    ).renderTo(d3.select("#scatterplot-plottable-demo"));
 });
+
